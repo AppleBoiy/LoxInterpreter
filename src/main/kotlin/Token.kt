e9204b48287b86f4b22d169eb421bc92b5e9f80b
Chaipat J. <work.chaipat@gmail.com>
2024-07-13 16:17:18 +0700

    codecrafters submit [skip ci]

--- src/main/kotlin/Token.kt
+++ src/main/kotlin/Token.kt
@@ -1,3 +1,25 @@
+enum class TokenType {
+    // Single-character tokens
+    STAR,
+    DOT,
+    COMMA,
+    PLUS,
+    MINUS,
+    SLASH,
+    SEMICOLON,
+    COLON,
+
+    // Parentheses and braces
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+
+    // Keywords
+    EOF,
+}
+
+
 data class Token(val type: TokenType, val value: String) {
     override fun toString(): String {
         return "$type $value null"
